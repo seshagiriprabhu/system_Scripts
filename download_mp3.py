d9b1b2361dce879fa87118b4dfdd2d7073725687
Seshagiri Prabhu <seshagiriprabhu@gmail.com>
2015-02-20 10:47:51 +0100

    Auto mp3 files download

---
+++ download_mp3.py
@@ -0,0 +1,60 @@
+import sys, re
+import urllib2
+import requests, urllib3, socket
+from BeautifulSoup import BeautifulSoup, SoupStrainer
+
+def download_file(url):
+    try:
+        file_name = url.split('/')[-1]
+        u = urllib2.urlopen(url)
+        f = open(file_name, 'wb')
+        meta = u.info()
+        file_size = int(meta.getheaders("Content-Length")[0])
+        print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+        file_size_dl = 0
+        block_sz = 8192
+        while True:
+            buffer = u.read(block_sz)
+            if not buffer:
+                break
+
+            file_size_dl += len(buffer)
+            f.write(buffer)
+            status = "%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+            status = status + chr(8)*(len(status)+1)
+            print status,
+
+        f.close()
+
+    except requests.exceptions.RequestException as e:
+        print "Exception", e
+
+
+def get_mp3_urls(source):
+    dom = BeautifulSoup(source, parseOnlyThese=SoupStrainer('a'))
+    tags = dom.findAll(href=re.compile("\.mp3$"))
+    mp3_url_list = []
+    for url in tags:
+        mp3_url_list.append(url.get('href'))
+    return mp3_url_list
+
+
+def download_all(url):
+    mp3_url_list = []
+    try:
+        r = requests.get(url)
+        mp3_url_list = get_mp3_urls(r.text)
+        for url in mp3_url_list:
+            download_file(url)
+    except requests.exceptions.RequestException as e:
+        print "Exception", e
+        sys.exit(1)
+        
+
+if len(sys.argv) < 2:
+    sys.exit("USAGE: \n%s [URL]" % sys.argv[0])
+else:
+    url = sys.argv[1]
+    print url 
+    download_all(url)
